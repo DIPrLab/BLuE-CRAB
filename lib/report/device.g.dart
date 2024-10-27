611d2cf0598c4e67788102204c8e528cf0c23aec
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-10-27 16:33:09 -0700

    Revert "Added MarkedSafe Field to Device"
    This reverts commit d8fd6fd4040d243ed30efc7aba33ef3f55b829e7.

--- lib/report/device.g.dart
+++ lib/report/device.g.dart
@@ -8,20 +8,15 @@ part of 'device.dart';
 
 Device _$DeviceFromJson(Map<String, dynamic> json) => Device(
       json['id'] as String,
-      name: json['name'] as String? ?? "",
-      platformName: json['platformName'] as String? ?? "",
-      manufacturer: (json['manufacturer'] as List<dynamic>?)?.map((e) => (e as num).toInt()).toList() ?? const [],
-      dataPoints:
-          (json['dataPoints'] as List<dynamic>?)?.map((e) => Datum.fromJson(e as Map<String, dynamic>)).toSet() ??
-              const {},
-      markedSafe: json['markedSafe'] as bool? ?? false,
-    );
+      json['name'] as String,
+      json['platformName'] as String,
+      (json['manufacturer'] as List<dynamic>).map((e) => (e as num).toInt()).toList(),
+    )..dataPoints = (json['dataPoints'] as List<dynamic>).map((e) => Datum.fromJson(e as Map<String, dynamic>)).toSet();
 
 Map<String, dynamic> _$DeviceToJson(Device instance) => <String, dynamic>{
       "\"id\"": "\"${instance.id}\"",
       "\"name\"": "\"${instance.name}\"",
       "\"platformName\"": "\"${instance.platformName}\"",
-      "\"markedSafe\"": instance.markedSafe,
       "\"manufacturer\"": instance.manufacturer,
       "\"dataPoints\"": instance.dataPoints.map((datum) => "${datum.toJson()}").toList(),
     };
