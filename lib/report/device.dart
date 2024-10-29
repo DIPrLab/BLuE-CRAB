3fe4fa7e88b771f0b72ec6c0248b731433723c3f
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-10-28 22:34:31 -0700

    Fixed Scan Button

--- lib/report/device.dart
+++ lib/report/device.dart
@@ -20,15 +20,9 @@ class Device {
   String id;
   String name;
   String platformName;
-  bool markedSafe;
   List<int> manufacturer;
-  Set<Datum> dataPoints;
-  Device(this.id,
-      {this.name = "",
-      this.platformName = "",
-      this.manufacturer = const [],
-      this.dataPoints = const {},
-      this.markedSafe = false});
+  Set<Datum> dataPoints = {};
+  Device(this.id, this.name, this.platformName, this.manufacturer);
   factory Device.fromJson(Map<String, dynamic> json) => _$DeviceFromJson(json);
   Map<String, dynamic> toJson() => _$DeviceToJson(this);
 
