f0707e3c4a0ada0cb6cfc659c1d38ba4b7b84eff
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-11-08 11:10:03 -0800

    Moved Private Stat Generators to Cache Extension

--- lib/report/report_cache.dart
+++ lib/report/report_cache.dart
@@ -9,4 +9,17 @@ extension Cache on Report {
     incidenceStats = _incidenceStats(devices, settings);
     areaStats = _areaStats(devices, settings);
   }
+
+  Stats _areaStats(Iterable<Device> devices, Settings settings) => Stats.fromData(
+      devices.map((device) => device.areas(settings.distanceThreshold(), settings.windowDuration()).length));
+
+  Stats _incidenceStats(Iterable<Device> devices, Settings settings) =>
+      Stats.fromData(devices.map((device) => device.incidence(settings.timeThreshold(), settings.windowDuration())));
+
+  Stats _timeTravelledStats(Iterable<Device> devices, Settings settings) => Stats.fromData(devices
+      .map((device) => device.timeTravelled(settings.timeThreshold(), settings.windowDuration()))
+      .map((duration) => duration.inSeconds));
+
+  Stats _distanceTravelledStats(Iterable<Device> devices, Settings settings) => Stats.fromData(
+      devices.map((device) => device.distanceTravelled(settings.timeThreshold(), settings.windowDuration())));
 }
--- lib/report/report_stats.dart
+++ lib/report/report_stats.dart
@@ -1,28 +1,10 @@
 part of 'report.dart';
 
 extension Statistics on Report {
-  num riskScore(Device device, Settings settings) {
-    Iterable<num> data = [
-      (device.timeTravelled(settings.timeThreshold(), settings.windowDuration()).inSeconds, timeTravelledStats),
-      (device.incidence(settings.timeThreshold(), settings.windowDuration()), incidenceStats),
-      (device.areas(settings.distanceThreshold(), settings.windowDuration()).length, areaStats),
-      (device.distanceTravelled(settings.timeThreshold(), settings.windowDuration()), distanceTravelledStats),
-    ].map((metric) => max(0, metric.$2.zScore(metric.$1)));
-    num avgResult = Stats.fromData(data).average;
-    // num addResult = data.reduce((a, b) => a + b);
-    return avgResult;
-  }
-
-  Stats _areaStats(Iterable<Device> devices, Settings settings) => Stats.fromData(
-      devices.map((device) => device.areas(settings.distanceThreshold(), settings.windowDuration()).length));
-
-  Stats _incidenceStats(Iterable<Device> devices, Settings settings) =>
-      Stats.fromData(devices.map((device) => device.incidence(settings.timeThreshold(), settings.windowDuration())));
-
-  Stats _timeTravelledStats(Iterable<Device> devices, Settings settings) => Stats.fromData(devices
-      .map((device) => device.timeTravelled(settings.timeThreshold(), settings.windowDuration()))
-      .map((duration) => duration.inSeconds));
-
-  Stats _distanceTravelledStats(Iterable<Device> devices, Settings settings) => Stats.fromData(
-      devices.map((device) => device.distanceTravelled(settings.timeThreshold(), settings.windowDuration())));
+  List<num> riskScores(Device device, Settings settings) => [
+        (device.timeTravelled(settings.timeThreshold(), settings.windowDuration()).inSeconds, timeTravelledStats),
+        (device.incidence(settings.timeThreshold(), settings.windowDuration()), incidenceStats),
+        (device.areas(settings.distanceThreshold(), settings.windowDuration()).length, areaStats),
+        (device.distanceTravelled(settings.timeThreshold(), settings.windowDuration()), distanceTravelledStats),
+      ].map((metric) => metric.$2.zScore(metric.$1)).toList();
 }
