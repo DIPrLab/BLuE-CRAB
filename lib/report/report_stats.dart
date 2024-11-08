06d002a5eff9c290f9491111d97dbdc909c04f24
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-11-08 11:46:26 -0800

    Update report_stats.dart

--- lib/report/report_stats.dart
+++ lib/report/report_stats.dart
@@ -1,10 +1,11 @@
 part of 'report.dart';
 
 extension Statistics on Report {
-  List<num> riskScores(Device device, Settings settings) => [
+  // TODO: Remove fold method and replace with device caching
+  num riskScore(Device device, Settings settings) => [
         (device.timeTravelled(settings.timeThreshold(), settings.windowDuration()).inSeconds, timeTravelledStats),
         (device.incidence(settings.timeThreshold(), settings.windowDuration()), incidenceStats),
         (device.areas(settings.distanceThreshold(), settings.windowDuration()).length, areaStats),
         (device.distanceTravelled(settings.timeThreshold(), settings.windowDuration()), distanceTravelledStats),
-      ].map((metric) => metric.$2.zScore(metric.$1)).toList();
+      ].map((metric) => metric.$2.zScore(metric.$1)).toList().fold(0, (a, b) => a + b);
 }
