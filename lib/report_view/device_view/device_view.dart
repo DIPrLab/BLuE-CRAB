8213fe8f428e56749a6c6ac141a18af7e7fafac1
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-11-08 23:57:12 -0800

    Update device_view.dart

--- lib/report_view/device_view/device_view.dart
+++ lib/report_view/device_view/device_view.dart
@@ -12,17 +12,19 @@ class DeviceView extends StatelessWidget {
 
   DeviceView(Device this.device, Settings this.settings, {super.key, required this.report});
 
+  Widget Tile(BuildContext context) => ListTile(
+      leading: CircleAvatar(backgroundColor: colors.altText, foregroundColor: colors.altText),
+      title: Expanded(
+          child: Text(device.deviceLabel() == device.id ? "" : device.deviceLabel(),
+              maxLines: 2, overflow: TextOverflow.ellipsis)),
+      subtitle: Expanded(child: Text(device.id, maxLines: 2, overflow: TextOverflow.ellipsis)),
+      trailing: Icon(Icons.keyboard_arrow_right),
+      onTap: () => Navigator.push(context,
+          MaterialPageRoute(builder: (context) => SafeArea(child: DeviceDetailView(device, report, settings)))));
+
   @override
   Widget build(BuildContext context) => Container(
       decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(10.0)), color: colors.foreground),
       padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
-      child: ListTile(
-          leading: CircleAvatar(backgroundColor: colors.altText, foregroundColor: colors.altText),
-          title: Expanded(
-              child: Text(device.deviceLabel() == device.id ? "" : device.deviceLabel(),
-                  maxLines: 2, overflow: TextOverflow.ellipsis)),
-          subtitle: Expanded(child: Text(device.id, maxLines: 2, overflow: TextOverflow.ellipsis)),
-          trailing: Icon(Icons.keyboard_arrow_right),
-          onTap: () => Navigator.push(context,
-              MaterialPageRoute(builder: (context) => SafeArea(child: DeviceDetailView(device, report, settings))))));
+      child: Tile(context));
 }
