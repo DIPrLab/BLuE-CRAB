cd46c3b2c06cd857ed9fe23972bc7ba9b5812d66
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-11-09 17:16:36 -0800

    Update styles.dart

--- lib/styles/styles.dart
+++ lib/styles/styles.dart
@@ -3,25 +3,17 @@ import 'package:bluetooth_detector/styles/themes.dart';
 
 class TextStyles {
   static var normal = const TextStyle();
-  static var splashText = const TextStyle(
-    fontSize: 64.0,
-    fontWeight: FontWeight.bold,
-  );
-  static var title = const TextStyle(
-    fontSize: 32.0,
-    fontWeight: FontWeight.bold,
-  );
+  static var splashText = const TextStyle(fontSize: 64.0, fontWeight: FontWeight.bold);
+  static var title = const TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold);
 }
 
 class AppButtonStyle {
   static ButtonStyle buttonWithBackground = ButtonStyle(
-    backgroundColor: WidgetStatePropertyAll(colors.foreground),
-    shape: WidgetStateProperty.all(const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(8.0)))),
-  );
+      backgroundColor: WidgetStatePropertyAll(colors.foreground),
+      shape:
+          WidgetStateProperty.all(const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8.0)))));
 
   static ButtonStyle buttonWithoutBackground = ButtonStyle(
-    shape: WidgetStateProperty.all(const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(8.0)))),
-  );
+      shape:
+          WidgetStateProperty.all(const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8.0)))));
 }
