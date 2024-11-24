8d374f0229570db9349465ab28f09052b24d65f5
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-11-24 00:34:56 -0800

    Added Values for Metrics to Settings

--- lib/settings.dart
+++ lib/settings.dart
@@ -5,10 +5,19 @@ class Settings {
   late bool devMode;
   late bool autoConnect;
   late bool locationEnabled;
+  late List<LatLng> safeZones;
+
+  // Risk Metrics
+  late bool enableAreasMetric;
+  late bool enableDistanceWithUserMetric;
+  late bool enableIncidenceMetric;
+  late bool enableRSSIMetric;
+  late bool enableTimeWithUserMetric;
+
+  // Properties that will be turned into calculated values
   late double windowDurationValue;
   late double timeThresholdValue;
   late double distanceThresholdValue;
-  late List<LatLng> safeZones;
 
   Duration windowDuration() => Duration(minutes: windowDurationValue.toInt());
   Duration scanTime() => Duration(seconds: 10);
@@ -20,24 +29,38 @@ class Settings {
         devMode = prefs.getBool("devMode") ?? false;
         autoConnect = prefs.getBool("autoConnect") ?? false;
         locationEnabled = prefs.getBool("locationEnabled") ?? true;
-        windowDurationValue = prefs.getDouble("windowDurationValue") ?? 10;
-        timeThresholdValue = prefs.getDouble("timeThreshold") ?? 10;
-        distanceThresholdValue = prefs.getDouble("distanceThreshold") ?? 10;
         safeZones = prefs.getStringList("safeZones")?.map((x) {
               List<String> latlng = x.split(',');
               return LatLng.degree(double.tryParse(latlng[0]) ?? 0.0, double.tryParse(latlng[1]) ?? 0.0);
             }).toList() ??
             [];
+
+        enableAreasMetric = prefs.getBool("enableAreasMetric") ?? true;
+        enableDistanceWithUserMetric = prefs.getBool("enableDistanceWithUserMetric") ?? true;
+        enableIncidenceMetric = prefs.getBool("enableIncidenceMetric") ?? true;
+        enableRSSIMetric = prefs.getBool("enableRSSIMetric") ?? true;
+        enableTimeWithUserMetric = prefs.getBool("enableTimeWithUserMetric") ?? true;
+
+        windowDurationValue = prefs.getDouble("windowDurationValue") ?? 10;
+        timeThresholdValue = prefs.getDouble("timeThresholdValue") ?? 10;
+        distanceThresholdValue = prefs.getDouble("distanceThresholdValue") ?? 10;
       });
 
   void save() => SharedPreferences.getInstance().then((prefs) {
         prefs.setBool("devMode", devMode);
         prefs.setBool("autoConnect", autoConnect);
         prefs.setBool("locationEnabled", locationEnabled);
-        prefs.setDouble("windowDurationValue", windowDurationValue);
-        prefs.setDouble("timeThreshold", timeThresholdValue);
-        prefs.setDouble("distanceThreshold", distanceThresholdValue);
         prefs.setStringList("safeZones",
             safeZones.map((z) => "${z.latitude.degrees.toString()},${z.longitude.degrees.toString()}").toList());
+
+        prefs.setBool("enableAreasMetric", enableAreasMetric);
+        prefs.setBool("enableDistanceWithUserMetric", enableDistanceWithUserMetric);
+        prefs.setBool("enableIncidenceMetric", enableIncidenceMetric);
+        prefs.setBool("enableRSSIMetric", enableRSSIMetric);
+        prefs.setBool("enableTimeWithUserMetric", enableTimeWithUserMetric);
+
+        prefs.setDouble("windowDurationValue", windowDurationValue);
+        prefs.setDouble("timeThresholdValue", timeThresholdValue);
+        prefs.setDouble("distanceThresholdValue", distanceThresholdValue);
       });
 }
