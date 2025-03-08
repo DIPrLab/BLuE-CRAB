cef0fada4f2490b3c84583ca6b40518ab2ff6476
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-03-07 21:54:45 -0800

    Update report_stats.dart

--- lib/report/report_stats.dart
+++ lib/report/report_stats.dart
@@ -8,4 +8,11 @@ extension Statistics on Report {
         (device.areas.length, areaStats),
         (device.distanceTravelled, distanceTravelledStats),
       ].map((metric) => metric.$2.zScore(metric.$1)).toList().fold(0, (a, b) => a + b);
+
+  List<num> riskScores(Device device) => [
+        (device.timeTravelled.inSeconds, timeTravelledStats),
+        (device.incidence, incidenceStats),
+        (device.areas.length, areaStats),
+        (device.distanceTravelled, distanceTravelledStats),
+      ].map((metric) => metric.$2.zScore(metric.$1)).toList();
 }
