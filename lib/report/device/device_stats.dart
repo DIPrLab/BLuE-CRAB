d59a39b7514d474d930c5fbab84e8029209fe956
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-03-31 02:40:52 -0700

    Update device_stats.dart

--- lib/report/device/device_stats.dart
+++ lib/report/device/device_stats.dart
@@ -22,8 +22,8 @@ extension DeviceStats on Device {
   double _distanceTravelled() => paths()
       .map((path) => path
           .mapOrderedPairs((pair) => distanceBetween(pair.$1.location, pair.$2.location))
-          .fold(0.0, (a, b) => a + b))
-      .fold(0.0, (a, b) => a + b);
+          .fold(0.toDouble(), (a, b) => a + b))
+      .fold(0.toDouble(), (a, b) => a + b);
 
   int _incidence() => _timeClusterPrefix().where((duration) => duration > Settings.shared.timeThreshold()).length + 1;
 
