c141b7e513847fbd1e04ca5c2b3fbe49507d7d3a
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-05-15 01:36:15 -0700

    Removed Unnecessary Code in Device Metrics Reporting

--- lib/testing_suite/device_metrics.dart
+++ lib/testing_suite/device_metrics.dart
@@ -2,18 +2,10 @@ part of 'package:blue_crab/testing_suite/testing_suite.dart';
 
 extension DeviceMetrics on TestingSuite {
   CSVData getDeviceMetrics(Report report) {
-    final CSVData csv = CSVData(["DEVICE_MAC", "TIME_WITH_USER", "INCIDENCE", "AREAS", "DISTANCE_WITH_USER"]);
-    final Map<String, Device> deviceEntries = {};
-    report.devices().forEach((d) {
-      deviceEntries[d.id] =
-          Device(d.id, d.name, d.platformName, d.manufacturer, dataPoints: d.dataPoints(testing: true).toSet());
-    });
-    final Report r = Report(deviceEntries);
-    if (r.devices().length < 2) {
-      return csv;
-    }
-    r.refreshCache();
-    r.devices().forEach((d) => csv.addRow([d.id, ...r.riskScores(d).map((e) => e.toString())]));
+    final CSVData csv = CSVData(["DEVICE_MAC", "TIME_WITH_USER", "DISTANCE_WITH_USER"]);
+    report
+      ..refreshCache()
+      ..devices().forEach((d) => csv.addRow([d.id, ...report.riskScores(d).map((e) => e.toString())]));
     return csv;
   }
 }
