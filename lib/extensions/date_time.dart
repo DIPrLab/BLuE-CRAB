10be8dbade28997922951f88b655db68d30d7c38
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-05-21 17:11:32 -0700

    Fix Bug Where Duration.toReadableString Returns Empty String

--- lib/extensions/date_time.dart
+++ lib/extensions/date_time.dart
@@ -1,8 +1,10 @@
 extension S on Duration {
-  String toReadableString() => [
-        "$inDays days",
-        "${(this - Duration(days: inDays)).inHours} hrs",
-        "${(this - Duration(hours: inHours)).inMinutes} mins",
-        "${(this - Duration(minutes: inMinutes)).inSeconds} sec"
-      ].where((s) => s[0] != "0").join(", ");
+  String toReadableString() => inSeconds < 1
+      ? "< 1 sec"
+      : [
+          "$inDays days",
+          "${(this - Duration(days: inDays)).inHours} hrs",
+          "${(this - Duration(hours: inHours)).inMinutes} mins",
+          "${(this - Duration(minutes: inMinutes)).inSeconds} sec"
+        ].where((s) => s[0] != "0").join(", ");
 }
