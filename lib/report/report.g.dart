90a4857a7fa9f8d4a80ae5d67a11ae9b27324179
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-05-28 18:29:56 -0700

    Update report.g.dart

--- lib/report/report.g.dart
+++ lib/report/report.g.dart
@@ -12,10 +12,12 @@ Report _$ReportFromJson(Map<String, dynamic> json) {
     report = Report(
         (json['data'] as Map<String, dynamic>).map((k, e) => MapEntry(k, Device.fromJson(e as Map<String, dynamic>))))
       ..time = DateTime.parse(json['time'] as String);
+    Logger().i("Successfully loaded report as Report");
   } catch (e) {
     Logger().w("Failed to load report as Report");
     try {
       report = BleDoubtReport.fromJson(json).toReport();
+      Logger().i("Successfully loaded report as BleDoubtReport");
     } catch (e) {
       Logger().w("Failed to load report as BleDoubtReport");
       Logger().i("Generating empty report");
