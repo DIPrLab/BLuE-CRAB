e214522b7e74d5b00790d30223fdcb403626b997
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-05-28 18:11:56 -0700

    Removed Battery Log Framework; Combined Related Filesystem Functions

--- lib/filesystem/battery_logs.dart
+++
@@ -1,5 +0,0 @@
-part of "package:blue_crab/filesystem/filesystem.dart";
-
-Future<File> get _localBatteryLog async => localFileDirectory.then((dir) => File("${dir.path}/battery_log.json"));
-
-Future<void> writeBatteryLog(String data) async => _localBatteryLog.then((file) => file.writeAsString(data));
--- lib/filesystem/filesystem.dart
+++ lib/filesystem/filesystem.dart
@@ -10,9 +10,24 @@ import 'package:logger/logger.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
 
-part "package:blue_crab/filesystem/report_file.dart";
-part "package:blue_crab/filesystem/battery_logs.dart";
-
 Future<Directory> get localFileDirectory async => getApplicationDocumentsDirectory();
 
 Settings readSettings() => Settings()..loadData();
+
+Future<File> get _localReportFile async => localFileDirectory.then((dir) => File("${dir.path}/reports.json"));
+
+Future<void> write(Report report) async => _localReportFile.then((file) => file.writeAsString("${report.toJson()}"));
+
+Future<Report> readReport() => (kDebugMode
+            ? rootBundle.loadString('assets/bledoubt_logs/bledoubt_log_g.json')
+            : _localReportFile.then((file) => file.readAsString()))
+        .then((jsonData) {
+      try {
+        return Report.fromJson(jsonDecode(jsonData));
+      } catch (e) {
+        Logger().e("Failed to load report");
+        return Report({});
+      }
+    });
+
+void shareReport() => _localReportFile.then((file) => Share.shareXFiles([XFile(file.path)]).then((_) {}));
