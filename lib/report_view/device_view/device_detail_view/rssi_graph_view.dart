57f63bd77ea09be5d1d83aed8833da9cea2d7546
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-07-08 23:17:02 -0700

    Simplified RSSI Graph View

--- lib/report_view/device_view/device_detail_view/rssi_graph_view.dart
+++ lib/report_view/device_view/device_detail_view/rssi_graph_view.dart
@@ -1,4 +1,5 @@
 import 'package:blue_crab/extensions/collections.dart';
+import 'package:blue_crab/report/datum.dart';
 import 'package:blue_crab/report/device/device.dart';
 import 'package:blue_crab/styles/styles.dart';
 import 'package:blue_crab/styles/themes.dart';
@@ -20,34 +21,34 @@ class RssiGraphView extends StatelessWidget {
       ]);
 
   Widget graph() {
-    final DateTime origin = device.dataPoints().map((e) => e.time).sorted((a, b) => a.compareTo(b)).first;
-    final List<double> timestamps =
-        device.dataPoints().map((e) => e.time.difference(origin).inSeconds.toDouble()).toList();
-    final List<num> actualRSSI = device.dataPoints().map((e) => e.rssi).toList();
-    final List<num> smoothedRSSI =
-        device.dataPoints().map((e) => e.rssi).toList().smoothedByExponentiallyWeightedMovingAverage(0.3).toList();
-    final List<num> smoothedRSSI2 =
-        device.dataPoints().map((e) => e.rssi).toList().smoothedByMovingAverage(3, SmoothingMethod.padding).toList();
-    final List<FlSpot> actualRSSIData =
-        List.generate(timestamps.length, (e) => (timestamps[e], actualRSSI[e].toDouble()))
-            .map((e) => FlSpot(e.$1, e.$2))
-            .toList();
-    final List<FlSpot> smoothedRSSIData =
-        List.generate(timestamps.length, (e) => (timestamps[e], smoothedRSSI[e].toDouble()))
-            .map((e) => FlSpot(e.$1, e.$2))
-            .toList();
-    final List<FlSpot> smoothedRSSIData2 =
-        List.generate(timestamps.length, (e) => (timestamps[e], smoothedRSSI2[e].toDouble()))
-            .map((e) => FlSpot(e.$1, e.$2))
-            .toList();
+    final List<FlSpot> smoothedData = device
+        .dataPoints()
+        .sorted((a, b) => a.time.compareTo(b.time))
+        .smoothedDatumByMovingAverage(const Duration(seconds: 15))
+        .map((e) => FlSpot(
+            e.time
+                .difference(device.dataPoints().map((e) => e.time).sorted((a, b) => a.compareTo(b)).first)
+                .inSeconds
+                .toDouble(),
+            e.rssi.toDouble()))
+        .toList();
+    final List<FlSpot> realData = device
+        .dataPoints()
+        .sorted((a, b) => a.time.compareTo(b.time))
+        .map((e) => FlSpot(
+            e.time
+                .difference(device.dataPoints().map((e) => e.time).sorted((a, b) => a.compareTo(b)).first)
+                .inSeconds
+                .toDouble(),
+            e.rssi.toDouble()))
+        .toList();
     return Scaffold(
         body: LineChart(
             LineChartData(minY: -100, maxY: 0, lineBarsData: [
-              LineChartBarData(dotData: const FlDotData(show: false), color: colors.foreground, spots: actualRSSIData),
-              LineChartBarData(dotData: const FlDotData(show: false), color: colors.altText, spots: smoothedRSSIData),
-              LineChartBarData(dotData: const FlDotData(show: false), color: colors.warnText, spots: smoothedRSSIData2),
+              LineChartBarData(dotData: const FlDotData(show: false), color: colors.safeText, spots: smoothedData),
+              LineChartBarData(dotData: const FlDotData(show: false), color: colors.warnText, spots: realData),
             ]),
-            curve: Curves.ease));
+            curve: Curves.easeInOutSine));
   }
 
   @override
