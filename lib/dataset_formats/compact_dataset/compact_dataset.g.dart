f9647e869f3addc5fd3bc9c285a95bd4d6ea1291
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-07-10 01:08:47 -0700

    Created CompactDataset

---
+++ lib/dataset_formats/compact_dataset/compact_dataset.g.dart
@@ -0,0 +1,73 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'compact_dataset.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CompactDataset _$CompactDatasetFromJson(Map<String, dynamic> json) =>
+    CompactDataset(
+      (json['devices'] as Map<String, dynamic>).map(
+        (k, e) => MapEntry(
+            k,
+            _$recordConvert(
+              e,
+              ($jsonValue) => (
+                $jsonValue[r'$1'] as String,
+                $jsonValue[r'$2'] as String,
+                ($jsonValue[r'$3'] as List<dynamic>)
+                    .map((e) => (e as num).toInt())
+                    .toList(),
+                ($jsonValue[r'$4'] as Map<String, dynamic>).map(
+                  (k, e) => MapEntry(
+                      DateTime.parse(k),
+                      (e as List<dynamic>)
+                          .map((e) => (e as num).toInt())
+                          .toList()),
+                ),
+              ),
+            )),
+      ),
+      (json['locationHistory'] as Map<String, dynamic>).map(
+        (k, e) => MapEntry(
+            DateTime.parse(k),
+            _$recordConvertNullable(
+              e,
+              ($jsonValue) => (
+                ($jsonValue[r'$1'] as num).toDouble(),
+                ($jsonValue[r'$2'] as num).toDouble(),
+              ),
+            )),
+      ),
+    );
+
+Map<String, dynamic> _$CompactDatasetToJson(CompactDataset instance) =>
+    <String, dynamic>{
+      'devices': instance.devices.map((k, e) => MapEntry(k, <String, dynamic>{
+            r'$1': e.$1,
+            r'$2': e.$2,
+            r'$3': e.$3,
+            r'$4': e.$4.map((k, e) => MapEntry(k.toIso8601String(), e)),
+          })),
+      'locationHistory': instance.locationHistory.map((k, e) => MapEntry(
+          k.toIso8601String(),
+          e == null
+              ? null
+              : <String, dynamic>{
+                  r'$1': e!.$1,
+                  r'$2': e!.$2,
+                })),
+    };
+
+$Rec _$recordConvert<$Rec>(
+  Object? value,
+  $Rec Function(Map) convert,
+) =>
+    convert(value as Map<String, dynamic>);
+
+$Rec? _$recordConvertNullable<$Rec>(
+  Object? value,
+  $Rec Function(Map) convert,
+) =>
+    value == null ? null : convert(value as Map<String, dynamic>);
