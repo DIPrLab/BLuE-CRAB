58a326503ce24ecca1368561596dc0f4fd940a77
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-07-10 21:47:59 -0700

    Simplified Report

--- lib/testing_suite/rssi_metric_data.dart
+++ lib/testing_suite/rssi_metric_data.dart
@@ -1,174 +1,32 @@
 part of 'package:blue_crab/testing_suite/testing_suite.dart';
 
 extension RssiMetricData on TestingSuite {
-  String getRssiMetricData(Report report, Set<String> gt, int factor) => [
-        (
-          "AVERAGE_AVERAGE",
-          report
-              .devices()
-              .map((device) => device.dataPoints())
-              .map((e) => e.map((datum) => datum.rssi))
-              .map((e) => e.toList().smoothedByMovingAverage(factor, SmoothingMethod.padding))
-              .map((e) => e.average)
-              .average
-        ),
-        (
-          "MEDIAN_AVERAGE",
-          report
-              .devices()
-              .map((device) => device.dataPoints())
-              .map((e) => e.map((datum) => datum.rssi))
-              .map((e) => e.toList().smoothedByMovingAverage(factor, SmoothingMethod.padding))
-              .map((e) => e.average)
-              .median()
-        ),
-        (
-          "MIN_AVERAGE",
-          report
-              .devices()
-              .map((device) => device.dataPoints())
-              .map((e) => e.map((datum) => datum.rssi))
-              .map((e) => e.toList().smoothedByMovingAverage(factor, SmoothingMethod.padding))
-              .map((e) => e.average)
-              .min
-        ),
-        (
-          "MAX_AVERAGE",
-          report
-              .devices()
-              .map((device) => device.dataPoints())
-              .map((e) => e.map((datum) => datum.rssi))
-              .map((e) => e.toList().smoothedByMovingAverage(factor, SmoothingMethod.padding))
-              .map((e) => e.average)
-              .max
-        ),
-        (
-          "AVERAGE_STD_DEV",
-          report
-              .devices()
-              .map((device) => device.dataPoints())
-              .map((e) => e.map((datum) => datum.rssi))
-              .map((e) => e.toList().smoothedByMovingAverage(factor, SmoothingMethod.padding))
-              .map((e) => e.standardDeviation())
-              .average
-        ),
-        (
-          "MEDIAN_STD_DEV",
-          report
-              .devices()
-              .map((device) => device.dataPoints())
-              .map((e) => e.map((datum) => datum.rssi))
-              .map((e) => e.toList().smoothedByMovingAverage(factor, SmoothingMethod.padding))
-              .map((e) => e.standardDeviation())
-              .median()
-        ),
-        (
-          "MIN_STD_DEV",
-          report
-              .devices()
-              .map((device) => device.dataPoints())
-              .map((e) => e.map((datum) => datum.rssi))
-              .map((e) => e.toList().smoothedByMovingAverage(factor, SmoothingMethod.padding))
-              .map((e) => e.standardDeviation())
-              .min
-        ),
-        (
-          "MAX_STD_DEV",
-          report
-              .devices()
-              .map((device) => device.dataPoints())
-              .map((e) => e.map((datum) => datum.rssi))
-              .map((e) => e.toList().smoothedByMovingAverage(factor, SmoothingMethod.padding))
-              .map((e) => e.standardDeviation())
-              .max
-        ),
-        (
-          "AVERAGE_STD_DEV_SUSPICIOUS",
-          report
-              .devices()
-              .where((device) => gt.contains(device.id))
-              .map((device) => device.dataPoints())
-              .map((e) => e.map((datum) => datum.rssi))
-              .map((e) => e.toList().smoothedByMovingAverage(factor, SmoothingMethod.padding))
-              .map((e) => e.standardDeviation())
-              .average
-        ),
-        (
-          "MEDIAN_STD_DEV_SUSPICIOUS",
-          report
-              .devices()
-              .where((device) => gt.contains(device.id))
-              .map((device) => device.dataPoints())
-              .map((e) => e.map((datum) => datum.rssi))
-              .map((e) => e.toList().smoothedByMovingAverage(factor, SmoothingMethod.padding))
-              .map((e) => e.standardDeviation())
-              .median()
-        ),
-        (
-          "MIN_STD_DEV_SUSPICIOUS",
-          report
-              .devices()
-              .where((device) => gt.contains(device.id))
-              .map((device) => device.dataPoints())
-              .map((e) => e.map((datum) => datum.rssi))
-              .map((e) => e.toList().smoothedByMovingAverage(factor, SmoothingMethod.padding))
-              .map((e) => e.standardDeviation())
-              .min
-        ),
-        (
-          "MAX_STD_DEV_SUSPICIOUS",
-          report
-              .devices()
-              .where((device) => gt.contains(device.id))
-              .map((device) => device.dataPoints())
-              .map((e) => e.map((datum) => datum.rssi))
-              .map((e) => e.toList().smoothedByMovingAverage(factor, SmoothingMethod.padding))
-              .map((e) => e.standardDeviation())
-              .max
-        ),
-        (
-          "AVERAGE_STD_DEV_NOT_SUSPICIOUS",
-          report
-              .devices()
-              .where((device) => !gt.contains(device.id))
-              .map((device) => device.dataPoints())
-              .map((e) => e.map((datum) => datum.rssi))
-              .map((e) => e.toList().smoothedByMovingAverage(factor, SmoothingMethod.padding))
-              .map((e) => e.standardDeviation())
-              .average
-        ),
-        (
-          "MEDIAN_STD_DEV_NOT_SUSPICIOUS",
-          report
-              .devices()
-              .where((device) => !gt.contains(device.id))
-              .map((device) => device.dataPoints())
-              .map((e) => e.map((datum) => datum.rssi))
-              .map((e) => e.toList().smoothedByMovingAverage(factor, SmoothingMethod.padding))
-              .map((e) => e.standardDeviation())
-              .median()
-        ),
-        (
-          "MIN_STD_DEV_NOT_SUSPICIOUS",
-          report
-              .devices()
-              .where((device) => !gt.contains(device.id))
-              .map((device) => device.dataPoints())
-              .map((e) => e.map((datum) => datum.rssi))
-              .map((e) => e.toList().smoothedByMovingAverage(factor, SmoothingMethod.padding))
-              .map((e) => e.standardDeviation())
-              .min
-        ),
-        (
-          "MAX_STD_DEV_NOT_SUSPICIOUS",
-          report
-              .devices()
-              .where((device) => !gt.contains(device.id))
-              .map((device) => device.dataPoints())
-              .map((e) => e.map((datum) => datum.rssi))
-              .map((e) => e.toList().smoothedByMovingAverage(factor, SmoothingMethod.padding))
-              .map((e) => e.standardDeviation())
-              .max
-        ),
-      ].map((e) => "${e.$1.padRight(30)} : ${e.$2}").join("\n");
+  String getRssiMetricData(Report report, Set<String> gt, int factor) {
+    final Iterable<List<num>> rssiPrefix = report
+        .devices()
+        .map((device) => device.dataPoints())
+        .map((e) => e.map((datum) => datum.rssi))
+        .map((e) => e.toList().smoothedByMovingAverage(factor, SmoothingMethod.padding));
+    final Iterable<double> rssiAverages = rssiPrefix.map((e) => e.average);
+    final Iterable<num> rssiStdDevs = rssiPrefix.map((e) => e.standardDeviation());
+
+    return [
+      ("AVERAGE_AVERAGE", rssiAverages.average),
+      ("MEDIAN_AVERAGE", rssiAverages.median()),
+      ("MIN_AVERAGE", rssiAverages.min),
+      ("MAX_AVERAGE", rssiAverages.max),
+      ("AVERAGE_STD_DEV", rssiStdDevs.average),
+      ("MEDIAN_STD_DEV", rssiStdDevs.median()),
+      ("MIN_STD_DEV", rssiStdDevs.min),
+      ("MAX_STD_DEV", rssiStdDevs.max),
+      ("AVERAGE_STD_DEV_SUSPICIOUS", rssiStdDevs.average),
+      ("MEDIAN_STD_DEV_SUSPICIOUS", rssiStdDevs.median()),
+      ("MIN_STD_DEV_SUSPICIOUS", rssiStdDevs.min),
+      ("MAX_STD_DEV_SUSPICIOUS", rssiStdDevs.max),
+      ("AVERAGE_STD_DEV_NOT_SUSPICIOUS", rssiStdDevs.average),
+      ("MEDIAN_STD_DEV_NOT_SUSPICIOUS", rssiStdDevs.median()),
+      ("MIN_STD_DEV_NOT_SUSPICIOUS", rssiStdDevs.min),
+      ("MAX_STD_DEV_NOT_SUSPICIOUS", rssiStdDevs.max),
+    ].map((e) => "${e.$1.padRight(30)} : ${e.$2}").join("\n");
+  }
 }
