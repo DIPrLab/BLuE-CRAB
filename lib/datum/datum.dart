95756ee030cc6562ed2bcca20c839f9fd2ee6d0f
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-10-07 23:07:32 -0700

    Update Datum constructor to handle time and rssi
    The Datum constructor now accepts an optional time parameter and ensures rssi does not exceed 0. This improves flexibility and enforces rssi value constraints.

--- lib/datum/datum.dart
+++ lib/datum/datum.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:blue_crab/extensions/date_time.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:latlng/latlng.dart';
@@ -5,8 +6,9 @@ import 'package:latlng/latlng.dart';
 /// Datum used to generate Data
 @JsonSerializable()
 class Datum {
-  Datum(this.location, this.rssi) {
-    time = DateTime.now().roundedToSecond();
+  Datum(this.location, this.rssi, {DateTime? time}) {
+    this.time = (time ?? DateTime.now()).roundedToSecond();
+    rssi = min(rssi, 0);
   }
   LatLng? location;
   int rssi;
