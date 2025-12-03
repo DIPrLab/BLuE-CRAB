f1533d3a4f1beb4edc5aacfc46af827e4bb4a5bb
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-12-02 18:27:10 -0800

    Update smoothing parameters and remove proximity from CSV
    Changed moving average window for 'SMOOTHED_MA_RESIZED' from 3 to 5 and removed 'DISTANCE_FROM_USER' column and proximity data from the CSV output in device_signal_information.dart.

--- lib/testing_suite/device_signal_information.dart
+++ lib/testing_suite/device_signal_information.dart
@@ -8,19 +8,17 @@ extension DeviceSignalInformation on TestingSuite {
       "SMOOTHED_EMA",
       "SMOOTHED_MA_PADDING",
       "SMOOTHED_MA_RESIZED",
-      "DISTANCE_FROM_USER",
     ]);
     final List<num> rssiValues = device.dataPoints().map((e) => e.rssi()).toList();
     final List<num> smoothEMA = rssiValues.smoothedByExponentiallyWeightedMovingAverage(0.7);
     final List<num> smoothMAPadding = rssiValues.smoothedByMovingAverage(5, SmoothingMethod.padding);
-    final List<num> smoothMAResizing = rssiValues.smoothedByMovingAverage(3, SmoothingMethod.resizing);
+    final List<num> smoothMAResizing = rssiValues.smoothedByMovingAverage(5, SmoothingMethod.resizing);
     device.dataPoints().indexed.forEach((e) => csv.addRow([
           e.$2.time.difference(device.dataPoints().first.time).inSeconds,
           e.$2.rssi(),
           smoothEMA[e.$1],
           smoothMAPadding[e.$1],
           smoothMAResizing[e.$1],
-          device.proximity()
         ].map((e) => e.toString()).toList()));
     return csv;
   }
