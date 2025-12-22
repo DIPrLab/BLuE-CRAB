8d005b5b736ee07b1fb74e5d8b207423348619eb
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-12-22 05:01:34 -0800

    Add distanceTo extension method to LatLng
    Introduces a DistanceBetween extension on LatLng, providing a distanceTo method for easier calculation of distance between two LatLng points.

--- lib/extensions/geolocator.dart
+++ lib/extensions/geolocator.dart
@@ -3,3 +3,7 @@ import 'package:latlng/latlng.dart';
 
 double distanceBetween(LatLng a, LatLng b) =>
     Geolocator.distanceBetween(a.latitude.degrees, a.longitude.degrees, b.latitude.degrees, b.longitude.degrees);
+
+extension DistanceBetween on LatLng {
+  double distanceTo(LatLng other) => distanceBetween(this, other);
+}
